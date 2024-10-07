95c48456de964af62005cc680a4e1317aaf62ff0
777hanh777hanh <thanhthientran151100@gmail.com>
2024-10-07 21:40:32 +0700

    feat: add user menu in header and button to switch theme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,30 +11,30 @@ const $$ = document.querySelectorAll.bind(document);
  * </script>
  */
 const loadComponent = (selector, path) => {
-    const content = localStorage.getItem(path);
+	const content = localStorage.getItem(path);
 
-    if (content) {
-        document.querySelector(selector).innerHTML = content;
-    }
+	if (content) {
+		document.querySelector(selector).innerHTML = content;
+	}
 
-    fetch(path)
-        .then((res) => res.text())
-        .then((html) => {
-            if (html !== content) {
-                document.querySelector(selector).innerHTML = html;
-                localStorage.setItem(path, html);
-            }
-        })
-        .finally(() => {
-            window.dispatchEvent(new Event('template-loaded'));
-        })
-        .catch(() => {
-            console.error(
-                'Could not load ' + `%c${selector}` + '%c component.',
-                'color: red;',
-                'color: inherit;',
-            );
-        });
+	fetch(path)
+		.then(res => res.text())
+		.then(html => {
+			if (html !== content) {
+				document.querySelector(selector).innerHTML = html;
+				localStorage.setItem(path, html);
+			}
+		})
+		.finally(() => {
+			window.dispatchEvent(new Event('template-loaded'));
+		})
+		.catch(() => {
+			console.error(
+				'Could not load ' + `%c${selector}` + '%c component.',
+				'color: red;',
+				'color: inherit;'
+			);
+		});
 };
 
 /**
@@ -42,21 +42,21 @@ const loadComponent = (selector, path) => {
  * có bị ẩn bởi display: none không
  */
 function isHidden(element) {
-    if (!element) return true;
+	if (!element) return true;
 
-    if (window.getComputedStyle(element).display === 'none') {
-        return true;
-    }
+	if (window.getComputedStyle(element).display === 'none') {
+		return true;
+	}
 
-    let parent = element.parentElement;
-    while (parent) {
-        if (window.getComputedStyle(parent).display === 'none') {
-            return true;
-        }
-        parent = parent.parentElement;
-    }
+	let parent = element.parentElement;
+	while (parent) {
+		if (window.getComputedStyle(parent).display === 'none') {
+			return true;
+		}
+		parent = parent.parentElement;
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -64,13 +64,13 @@ function isHidden(element) {
  * sau một khoảng thời gian mới được thực thi
  */
 function debounce(func, timeout = 300) {
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            func.apply(this, args);
-        }, timeout);
-    };
+	let timer;
+	return (...args) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			func.apply(this, args);
+		}, timeout);
+	};
 }
 
 /**
@@ -81,14 +81,14 @@ function debounce(func, timeout = 300) {
  * 2. CSS "left" cho arrow qua biến "--arrow-left-pos"
  */
 const calArrowPos = debounce(() => {
-    if (isHidden($('.js-dropdown-list'))) return;
+	if (isHidden($('.js-dropdown-list'))) return;
 
-    const items = $$('.js-dropdown-list > li');
+	const items = $$('.js-dropdown-list > li');
 
-    items.forEach((item) => {
-        const arrowPos = item.offsetLeft + item.offsetWidth / 2;
-        item.style.setProperty('--arrow-left-pos', `${arrowPos}px`);
-    });
+	items.forEach(item => {
+		const arrowPos = item.offsetLeft + item.offsetWidth / 2;
+		item.style.setProperty('--arrow-left-pos', `${arrowPos}px`);
+	});
 });
 
 // Tính toán lại vị trí arrow khi resize trình duyệt
@@ -109,51 +109,51 @@ window.addEventListener('template-loaded', handleActiveMenu);
 window.addEventListener('resize', handleActiveMenu);
 
 function handleActiveMenu() {
-    const dropdowns = $$('.js-dropdown');
-    const menus = $$('.js-menu-list');
-    const activeClass = 'menu-column__item--active';
+	const dropdowns = $$('.js-dropdown');
+	const menus = $$('.js-menu-list');
+	const activeClass = 'menu-column__item--active';
 
-    const removeActive = (menu) => {
-        menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
-    };
+	const removeActive = menu => {
+		menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
+	};
 
-    const init = () => {
-        menus.forEach((menu) => {
-            const items = menu.children;
-            if (!items.length) return;
+	const init = () => {
+		menus.forEach(menu => {
+			const items = menu.children;
+			if (!items.length) return;
 
-            removeActive(menu);
-            if (window.innerWidth > 991) items[0].classList.add(activeClass);
+			removeActive(menu);
+			if (window.innerWidth > 991) items[0].classList.add(activeClass);
 
-            let currentItem = null;
+			let currentItem = null;
 
-            Array.from(items).forEach((item) => {
-                item.onmouseenter = () => {
-                    if (window.innerWidth <= 991) return;
-                    removeActive(menu);
-                    item.classList.add(activeClass);
-                };
-                item.onclick = (e) => {
-                    if (window.innerWidth > 991) return;
-                    removeActive(menu);
-                    if (!e.target.closest('.sub-menu') && currentItem === item) {
-                        currentItem.classList.remove(activeClass);
-                        currentItem = null;
-                    } else {
-                        item.classList.add(activeClass);
-                        item.scrollIntoView({ behavior: 'smooth' });
-                        currentItem = item;
-                    }
-                };
-            });
-        });
-    };
+			Array.from(items).forEach(item => {
+				item.onmouseenter = () => {
+					if (window.innerWidth <= 991) return;
+					removeActive(menu);
+					item.classList.add(activeClass);
+				};
+				item.onclick = e => {
+					if (window.innerWidth > 991) return;
+					removeActive(menu);
+					if (!e.target.closest('.sub-menu') && currentItem === item) {
+						currentItem.classList.remove(activeClass);
+						currentItem = null;
+					} else {
+						item.classList.add(activeClass);
+						item.scrollIntoView({ behavior: 'smooth' });
+						currentItem = item;
+					}
+				};
+			});
+		});
+	};
 
-    init();
+	init();
 
-    dropdowns.forEach((dropdown) => {
-        dropdown.onmouseleave = () => init();
-    });
+	dropdowns.forEach(dropdown => {
+		dropdown.onmouseleave = () => init();
+	});
 }
 
 /**
@@ -163,30 +163,12 @@ function handleActiveMenu() {
  */
 
 function initModeInterface() {
-    const darkMode = JSON.parse(localStorage.getItem('dark-mode'));
+	const darkMode = JSON.parse(localStorage.getItem('dark-mode'));
 
-    if (darkMode) {
-        document.documentElement.classList.add('dark');
-    }
+	if (darkMode) {
+		document.documentElement.classList.add('dark');
+	}
 }
-
-function handleModeInterface() {
-    const darkModeToggle = $('.js-dark-mode-toggle');
-
-    darkModeToggle.onclick = () => {
-        document.documentElement.classList.toggle('dark');
-        const currentMode = JSON.parse(localStorage.getItem('dark-mode'));
-        const newMode = !currentMode;
-        console.log('Dark mode:', newMode);
-        localStorage.setItem('dark-mode', newMode.toString());
-    };
-}
-
-// Khởi tạo giao diện dark/light
-initModeInterface();
-
-// Thêm sự kiện chuyển đổi giao diện dark/light khi DOM sẵn sàng
-window.addEventListener('DOMContentLoaded', handleModeInterface);
 
 /**
  * JS toggle
@@ -198,70 +180,99 @@ window.addEventListener('DOMContentLoaded', handleModeInterface);
 window.addEventListener('template-loaded', initJsToggle);
 
 function initJsToggle() {
-    $$('.js-toggle').forEach((button) => {
-        const target = button.getAttribute('toggle-target');
-        if (!target) {
-            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
-        }
-        button.onclick = (e) => {
-            e.preventDefault();
+	$$('.js-toggle').forEach(button => {
+		const target = button.getAttribute('toggle-target');
+		if (!target) {
+			document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+		}
+		button.onclick = e => {
+			e.preventDefault();
 
-            if (!$(target)) {
-                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
-            }
-            const isHidden = $(target).classList.contains('hide');
+			if (!$(target)) {
+				return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+			}
+			const isHidden = $(target).classList.contains('hide');
 
-            requestAnimationFrame(() => {
-                $(target).classList.toggle('hide', !isHidden);
-                $(target).classList.toggle('show', isHidden);
-            });
-        };
-        // Ẩn box khi click ra ngoài
-        document.onclick = function (e) {
-            // Nếu click vào phần tử không thuộc target
-            if (!e.target.closest(target)) {
-                // Kiểm tra xem target có đang hiển thị không
-                const isHidden = $(target).classList.contains('hide');
-                // Nếu đang hiển thị thì ẩn đi
-                if (!isHidden) {
-                    button.click();
-                }
-            }
-        };
-    });
+			requestAnimationFrame(() => {
+				$(target).classList.toggle('hide', !isHidden);
+				$(target).classList.toggle('show', isHidden);
+			});
+		};
+		// Ẩn box khi click ra ngoài
+		document.onclick = function (e) {
+			// Nếu click vào phần tử không thuộc target
+			if (!e.target.closest(target)) {
+				// Kiểm tra xem target có đang hiển thị không
+				const isHidden = $(target).classList.contains('hide');
+				// Nếu đang hiển thị thì ẩn đi
+				if (!isHidden) {
+					button.click();
+				}
+			}
+		};
+	});
 }
 
 window.addEventListener('template-loaded', () => {
-    const links = $$('.js-dropdown-list > li > a');
+	const links = $$('.js-dropdown-list > li > a');
 
-    links.forEach((link) => {
-        link.onclick = () => {
-            if (window.innerWidth > 1199.98) return;
-            const item = link.closest('li');
-            item.classList.toggle('navbar__item--active');
-        };
-    });
+	links.forEach(link => {
+		link.onclick = () => {
+			if (window.innerWidth > 1199.98) return;
+			const item = link.closest('li');
+			item.classList.toggle('navbar__item--active');
+		};
+	});
 });
 
+window.addEventListener('template-loaded', () => {
+	const tabsSelector = 'product-tab__item';
+	const contentsSelector = 'product-tab__content';
 
-window.addEventListener("template-loaded", () => {
-    const tabsSelector = "product-tab__item";
-    const contentsSelector = "product-tab__content";
+	const tabActive = `${tabsSelector}--current`;
+	const contentActive = `${contentsSelector}--current`;
 
-    const tabActive = `${tabsSelector}--current`;
-    const contentActive = `${contentsSelector}--current`;
-
-    const tabContainers = $$(".js-tabs");
-    tabContainers.forEach((tabContainer) => {
-        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
-        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
-        tabs.forEach((tab, index) => {
-            tab.onclick = () => {
-                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
-                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
-                tab.classList.add(tabActive);
-                contents[index].classList.add(contentActive);
-            };
-        });
-    });
+	const tabContainers = $$('.js-tabs');
+	tabContainers.forEach(tabContainer => {
+		const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+		const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+		tabs.forEach((tab, index) => {
+			tab.onclick = () => {
+				tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+				tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+				tab.classList.add(tabActive);
+				contents[index].classList.add(contentActive);
+			};
+		});
+	});
 });
+
+window.addEventListener('template-loaded', () => {
+	const switchBtns = document.querySelectorAll('.switch-theme-btn');
+	if (switchBtns) {
+		switchBtns.forEach(switchBtn => {
+			switchBtn.onclick = function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+
+				const isDark = localStorage.dark === 'true';
+				document.querySelector('html').classList.toggle('dark', !isDark);
+				localStorage.setItem('dark', !isDark);
+				if (switchBtn.querySelector('span')) {
+					switchBtn.querySelector('span').textContent = isDark
+						? 'Dark mode'
+						: 'Light mode';
+				}
+
+				switchBtn.querySelectorAll('img').forEach(img => {
+					img.classList.toggle('hide');
+				});
+			};
+			const isDark = localStorage.dark === 'true';
+			switchBtn.querySelector('span').textContent = isDark ? 'Light mode' : 'Dark mode';
+		});
+	}
+});
+
+const isDark = localStorage.dark === 'true';
+document.querySelector('html').classList.toggle('dark', isDark);
